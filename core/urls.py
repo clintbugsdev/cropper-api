de3b596e7e678ffa5fc45b7e0b381fd66ee55de7
Clinton Bugtong <clintbugs.dev@gmail.com>
2022-06-18 22:01:52 +0800

    Added JWT Token

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from core.views import RegisterAPIView
-
+from core.views import RegisterAPIView, LoginAPIView, UserAPIView, RefreshAPIView
 
 urlpatterns = [
     path('register', RegisterAPIView.as_view()),
+    path('login', LoginAPIView.as_view()),
+    path('user', UserAPIView.as_view()),
+    path('refresh', RefreshAPIView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .authentication import create_access_token, create_refresh_token, JWTAuthentication, decode_refresh_token
+from .models import User
 from .serializers import UserSerializer
 
 
@@ -17,3 +20,47 @@ class RegisterAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        access_token = create_access_token(user.id)
+        refresh_token = create_refresh_token(user.id)
+
+        response = Response()
+        response.set_cookie(key='refresh_token', value=refresh_token, httponly=True)
+        response.data = {
+            'token': access_token
+        }
+        return response
+
+
+class UserAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
+
+
+class RefreshAPIView(APIView):
+
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token')
+        id = decode_refresh_token(refresh_token)
+
+        access_token = create_access_token(id)
+        return Response({
+            'token': access_token
+        })
